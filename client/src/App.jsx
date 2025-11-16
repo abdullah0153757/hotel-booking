8165fdb64bf26d0294cf30b422ec53eb20d3e288
Mohammad Abdullah <abdullah0153757@gmail.com>
2025-11-16 20:44:24 +0600

    fonted complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,9 +8,10 @@ import RoomDetails from './pages/RoomDetails';
 import MyBookings from './pages/MyBookings';
 import HotelReg from './components/HotelReg';
 import Layout from './pages/hotelOwner/Layout';
+import Dashboard from './pages/hotelOwner/Dashboard';
 import AddRoom from './pages/hotelOwner/AddRoom';
 import ListRoom from './pages/hotelOwner/ListRoom';
-import Dashboard from './pages/hotelOwner/Dashboard';
+
 
 
 const App = () => {
@@ -28,6 +29,12 @@ const App = () => {
           <Route path='/rooms' element={<AllRooms /> } />
           <Route path='/rooms/:id' element={<RoomDetails /> } />
           <Route path='/my-bookings' element={<MyBookings /> } />
+          <Route path='/owner' element={<Layout/>}>
+              <Route index element={<Dashboard/>} />
+              <Route path='add-room' element={<AddRoom/>} />
+              <Route path='list-room' element={<ListRoom/>} />
+
+          </Route>
           
         </Routes>
       </div>
